f28e3f212d6cf67897e5a23577915cb5deddf9e8
Miles McLenon <miles.mclenon@gmail.com>
2024-11-13 09:01:01 -0600

    Add base sepolia to config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const SEPOLIA_PRIVATE_KEY = vars.get("BASE_SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    base_sepolia: {
+      url: 'https://sepolia.base.org',
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
