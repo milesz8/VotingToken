c31ef4bbeb43c13e3a675c7d8abcabe22adafbd5
Miles McLenon <miles.mclenon@gmail.com>
2024-11-14 20:51:09 -0600

    update readme

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
 const SEPOLIA_PRIVATE_KEY = vars.get("BASE_SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
@@ -9,6 +11,10 @@ const config: HardhatUserConfig = {
       url: 'https://sepolia.base.org',
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
   },
 };
 
