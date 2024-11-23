ecc2215c035d1ca366ba30270557223bffd1ac49
Miles McLenon <miles.mclenon@gmail.com>
2024-11-23 10:40:13 -0600

    Set up sst to deploy to domain

--- voting-app/sst.config.ts
+++ voting-app/sst.config.ts
@@ -1,14 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "voting-app",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: { cloudflare: "5.43.1" },
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyWeb");
+    new sst.aws.Nextjs("VotingAppWeb", {
+      domain: {
+        name: "voting-app.milesmclenon.com",
+        dns: sst.cloudflare.dns(),
+      },
+    });
   },
 });
